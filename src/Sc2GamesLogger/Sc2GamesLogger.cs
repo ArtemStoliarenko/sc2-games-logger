eff9d22846ca161edc80f56fcddb8342188f4dd4
artem.stolyarenko <roguenalan@gmail.com>
2018-08-05 14:08:52 +0300

    Initialized repository

---
+++ src/Sc2GamesLogger/Sc2GamesLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sc2GamesLogger
+{
+    public partial class Sc2GamesLogger : Form
+    {
+        public Sc2GamesLogger()
+        {
+            InitializeComponent();
+        }
+    }
+}
